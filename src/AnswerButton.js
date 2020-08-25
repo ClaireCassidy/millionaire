fb93e1d667104dff8890d366b58e853390dd0dae
ClaireCassidy <clairecassidy0@hotmail.com>
2020-08-25 19:01:01 +0100

    disabled debug

--- src/AnswerButton.js
+++ src/AnswerButton.js
@@ -4,7 +4,7 @@ import './AnswerButton.css'
 export default function AnswerButton({answer, isCorrect, selectAnswer, disabled}) {
     return (
     	<div className="AnswerButtonContainer">
-            <button className={"AnsButton "+(isCorrect ? "_DebugCorrect" : "")} onClick={() => {selectAnswer(isCorrect)}} disabled={disabled}>{answer}</button>
+            <button className={"AnsButton "} onClick={() => {selectAnswer(isCorrect)}} disabled={disabled}>{answer}</button>
         </div>
     )
 }
